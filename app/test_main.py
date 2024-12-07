dec64f9803956202d5bfe594f4e7873f18b719a0
Глеб Дубинин <gleb1395@gmail.com>
2024-12-07 23:27:42 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -26,17 +26,17 @@ def data_groceries() -> list:
     ]
 
 
-def test_check_first_outdated_product(data_groceries):
+def test_check_first_outdated_product(data_groceries: list) -> None:
     result = outdated_products(data_groceries)
     assert result == ["salmon", "chicken", "duck"]
 
 
-def test_check_empty_groceries():
+def test_check_empty_groceries() -> None:
     result = outdated_products([])
     assert result == []
 
 
-def test_check_valid_groceries(data_groceries):
+def test_check_valid_groceries(data_groceries: list) -> None:
     data_groceries[0]["expiration_date"] = datetime.date(2025, 2, 3)
     data_groceries[1]["expiration_date"] = datetime.date(2027, 2, 3)
     data_groceries[2]["expiration_date"] = datetime.date(2026, 2, 3)
@@ -44,7 +44,7 @@ def test_check_valid_groceries(data_groceries):
     assert result == []
 
 
-def test_check_invalid_expiration_date(data_groceries):
+def test_check_invalid_expiration_date(data_groceries: list) -> None:
     data_groceries[0]["expiration_date"] = datetime.date.today()
     data_groceries[1]["expiration_date"] = datetime.date.today()
     data_groceries[2]["expiration_date"] = datetime.date.today()
@@ -52,7 +52,7 @@ def test_check_invalid_expiration_date(data_groceries):
     assert result == []
 
 
-def test_check_expiration_date_equals_yesterday(data_groceries):
+def test_check_expiration_date_equals_yesterday(data_groceries: list) -> None:
     today = datetime.date.today()
     yesterday = today - datetime.timedelta(days=1)
     data_groceries[0]["expiration_date"] = yesterday
